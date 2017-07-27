d8f6cfb07fb080b2501564f4163bfb4b1e9c0c6d
James Fowkes <jamesfowkes@gmail.com>
2017-07-27 17:01:06 +0100

    Wipe test DB before each scenario

--- aloe-test/features/__init__.py
+++ aloe-test/features/__init__.py
@@ -7,6 +7,8 @@ os.environ["GF_CONFIG_CLASS"] = "config.AloeConfig"
 from app.application import app
 from app.database import db
 from app.models.user import User
+from app.models.course import Course
+from app.models.category import Category
 
 @aloe.before.each_example
 def before_each_scenario(scenario, outline, steps):
@@ -15,6 +17,12 @@ def before_each_scenario(scenario, outline, steps):
 
     for user in User.get():
         user.delete()
+
+    for course in Course.get():
+        course.delete()
+
+    for category in Category.get():
+        category.delete()
 
     User.create('standard', 'Standard Smith', 'standard', False)
     User.create('admin', 'Admin Smith', 'admin', True)
