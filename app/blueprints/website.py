8125623253aac36ce7fc286fe9eb37a7c5268af1
James Fowkes <jamesfowkes@gmail.com>
2017-09-29 21:39:39 +0100

    Basic translation support in routing and templates

--- app/blueprints/website.py
+++ app/blueprints/website.py
@@ -1,13 +1,29 @@
-from flask import Blueprint
 import logging
 
-from flask import render_template
+from flask import Blueprint, g, render_template, request, redirect
 
-website = Blueprint('website', __name__, template_folder='templates')
+from app import app
+from app.locale import get_locale
+
 logger = logging.getLogger(__name__)
 
+website = Blueprint('website', __name__, template_folder='templates', url_prefix='/<lang>')
+
+@website.url_defaults
+def add_language_code(endpoint, values):
+    values.setdefault('lang', g.lang)
+
+@website.url_value_preprocessor
+def pull_lang(endpoint, values):
+    requested_language = values.pop('lang')
+    logger.info("Requested: %s", requested_language)
+    if requested_language in app.config["SUPPORTED_LOCALES"]:
+        g.lang = requested_language
+    else:
+        g.lang = get_locale()
+        logger.info("%s not supported, using %s from get_locale", requested_language, g.lang)
+
 @website.route("/", methods=['GET'])
-@website.route("/index", methods=['GET'])
 def render_index():
     return render_template('index.tpl')
 
--- app/locale.py
+++ app/locale.py
@@ -2,7 +2,7 @@ import logging
 
 import sqlalchemy_i18n
 import sqlalchemy_utils
-from flask_babel import Babel
+from flask_babel import Babel, gettext
 
 from flask import request, g
 
@@ -14,19 +14,16 @@ babel = Babel()
 
 @babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(app.config["SUPPORTED_LOCALES"])
+    try:
+        locale = g.lang
+    except:
+        locale = request.accept_languages.best_match(app.config["SUPPORTED_LOCALES"])
 
-def before():
-    if request.view_args and 'lang_code' in request.view_args:
-        if request.view_args['lang_code'] not in app.config["SUPPORTED_LOCALES"]:
-            g.current_lang = 'en'
-        g.current_lang = request.view_args['lang_code']
-        request.view_args.pop('lang_code')
-    else:
-        g.current_lang = 'en'
+    logger.info(locale)
+
+    return locale
     
 def init_app(app):
-    #app.before_request(before)
     babel.init_app(app)
     sqlalchemy_utils.i18n.get_locale = get_locale
     sqlalchemy_i18n.make_translatable(options={'locales': app.config["SUPPORTED_LOCALES"]})
